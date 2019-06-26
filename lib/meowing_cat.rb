f8ffe497496dda0e967dac899161a16f7d44fe3d
github username <github email address>
2019-06-26 17:46:59 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
 ## code your solution here. 
+# Create a class of Cat.
+class Cat
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+
+end
+
+maru = Cat.new
+
+maru.name
+
+maru.meow
